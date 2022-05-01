db92b174935e2bb1ab51e9dbafc3d5e57fd0adb4
omar-ameen1 <mierzofdoom@gmail.com>
2022-05-01 05:38:42 -0400

    DEPLOY

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "popper.js"
+import Popper from "popper.js"
 import "bootstrap"
 import * as bootstrap from "bootstrap"
